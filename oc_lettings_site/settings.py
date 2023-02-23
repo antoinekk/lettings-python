5e19953b44bad67182df5b6f5eb9f5ee35cdab5b
antoine <kronek.antoine@gmail.com>
2023-02-23 15:55:26 +0100

    urls configuration and old tables deletion

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,9 +19,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'oc_lettings_site.apps.OCLettingsSiteConfig',
     'lettings.apps.LettingsConfig',
     'profiles.apps.ProfilesConfig',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
