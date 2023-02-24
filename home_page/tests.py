d6a74687390678228b7dc8cefd276271cc2fa62d
antoine <kronek.antoine@gmail.com>
2023-02-24 10:33:56 +0100

    add test for home page

--- home_page/tests.py
+++ home_page/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_index_home(self):
+        response = self.client.get(reverse('home_page:index'))
+        assert b"<h1>Welcome to Holiday Homes</h1>" in response.content
+        assert response.status_code == 200
+
