cbf95523460aa86dd6948da8a69dd472d83fffa1
antoine <kronek.antoine@gmail.com>
2023-02-24 16:18:43 +0100

    tests for indexes views

--- lettings/tests.py
+++ lettings/tests.py
@@ -2,5 +2,9 @@ from django.test import TestCase
 from django.urls import reverse
 
 class LettingsTest(TestCase):
-    pass
+    
+    def test_index_lettings(self):
+        response = self.client.get(reverse('lettings:index'))
+        assert b"<title>Lettings</title>" in response.content
+        assert response.status_code == 200
 
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ProfilesTest(TestCase):
+    
+    def test_index_profiles(self):
+        response = self.client.get(reverse('profiles:index'))
+        assert b"<title>Profiles</title>" in response.content
+        assert response.status_code == 200
