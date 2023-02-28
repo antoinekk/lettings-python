07a114febc7bff98568e2da356de945f475952c1
antoine <kronek.antoine@gmail.com>
2023-02-28 10:27:43 +0100

    last tests for lettings and profiles

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import Letting
 
 class LettingsTest(TestCase):
-    
+
     def test_index_lettings(self):
         response = self.client.get(reverse('lettings:index'))
         assert b"<title>Lettings</title>" in response.content
         assert response.status_code == 200
+
+    def test_letting(self):
+        lettings = Letting.objects.all()
+        for letting in lettings:
+            response = self.client.get(reverse('lettings:letting', args=[letting.id]))
+            assert letting.title in response.content
+            assert response.status_code == 200
+
 
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import Profile
 
 class ProfilesTest(TestCase):
     
@@ -7,3 +8,10 @@ class ProfilesTest(TestCase):
         response = self.client.get(reverse('profiles:index'))
         assert b"<title>Profiles</title>" in response.content
         assert response.status_code == 200
+    
+    def test_profile(self):
+        profiles = Profile.objects.all()
+        for profile in profiles:
+            response = self.client.get(reverse('profiles:profile', args=[profile.id]))
+            assert profile.title in response.content
+            assert response.status_code == 200
