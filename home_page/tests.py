aee7d90a13dd9c30970168766f5c7047c185a618
antoine <kronek.antoine@gmail.com>
2023-03-24 14:21:16 +0100

    test2

--- home_page/tests.py
+++ home_page/tests.py
@@ -7,4 +7,4 @@ class HomePageTest(TestCase):
     def test_index_home(self):
         response = self.client.get(reverse('home_page:index'))
         assert b"<h1>Welcome to Holiday Homes</h1>" in response.content
-        assert response.status_code == 200
+        assert response.status_code == 404
